26c6baccfe5577b22fe0db3d2f8aa856591d23c8
Matric Bastos <matricbts@gmail.com>
2022-04-20 01:29:53 -0300

    criando classe das peças

---
+++ src/models/Peças.model.js
@@ -0,0 +1,12 @@
+class Peças {
+    constructor(nome, preço, categoria, quantidade, marca, garantia){
+        this.nome = nome,
+        this.preço = preço,
+        this.categoria = categoria,
+        this.quantidade = quantidade,
+        this.marca = marca,
+        this.garantia = garantia
+    }
+}
+
+module.exports = Peças;
