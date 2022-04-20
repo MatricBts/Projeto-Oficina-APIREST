1c488be2e8d1f522079de04b7a116b6f0c1c8309
Matric Bastos <matricbts@gmail.com>
2022-04-20 19:10:29 -0300

    adicionando banco temporario

---
+++ infra/db.js
@@ -0,0 +1,20 @@
+const db = [
+    {
+        nome: 'filtro oleo',
+        preço: 15,
+        categoria: 'revisao',
+        quantidade: 10,
+        marca: 'wega',
+        garantia: 0
+    },
+    {
+        nome: 'terminal direcao',
+        preço: 35,
+        categoria: 'suspensao',
+        quantidade: 6,
+        marca: 'nakata',
+        garantia: 90
+    }
+]
+
+module.exports = db;
--- src/routes/peças.routes.js
+++ src/routes/peças.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router();
+const db = require('./../../infra/db.js')
 
 router
-    .get('/pecas', (req,res) => res.send('foi'))
+    .get('/pecas', (req,res) => {
+        res.json(db)
+    })
     .post('/pecas', (req, res) => res.send('post'))
     .put('/pecas', (req, res) => res.send('post'))
     .delete('/pecas', (req, res) => res.send('post'))
