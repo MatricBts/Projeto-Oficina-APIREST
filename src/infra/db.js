117f9a0e8d113d73c2a31dc7da2f01dd39fe0b44
Matric Bastos <matricbts@gmail.com>
2022-04-26 10:35:10 -0300

    melhorando identação

--- src/infra/db.js
+++ src/infra/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('oficina', 'root', 'pass', {
-    dialect:'sqlite',
-    host:'./src/infra/database.db'
+  dialect:'sqlite',
+  host:'./src/infra/database.db'
 })
 
 module.exports = sequelize;
--- src/routes/pecas.routes.js
+++ src/routes/pecas.routes.js
@@ -3,28 +3,28 @@ const router = express.Router();
 const pecasControllers = require('../controllers/pecas.controllers')
 
 router
-    .get('/pecas', async (req,res) => {
-      await pecasControllers.mostrarPeças(res);
-    })
-    .get('/pecas/atributos', async (req,res) => {
-        const {body: dados} = req
-        await pecasControllers.encontrarPorAtributos(dados, res);
-    })
-    .post('/pecas', async (req, res) => {
-      const {body} = req
-      console.log(body)
-      pecasControllers.cadastrar(body, res);
-    })
-    .post('/pecas/populate', async (req, res)=>{
-      await pecasControllers.popularTabela(res)
-    })
-    .put('/pecas', async (req, res) => {
-      const {body: {dados, dadosNovos}} = req;
-      await pecasControllers.atualizar( dados, dadosNovos, res)
-    })
-    .delete('/pecas', (req, res) => {
-      const {body: dados} = req
-      pecasControllers.deletar(dados, res);
-    })
+  .get('/pecas', async (req,res) => {
+    await pecasControllers.mostrarPeças(res);
+  })
+  .get('/pecas/atributos', async (req,res) => {
+    const {body: dados} = req
+    await pecasControllers.encontrarPorAtributos(dados, res);
+  })
+  .post('/pecas', async (req, res) => {
+    const {body} = req
+    console.log(body)
+    pecasControllers.cadastrar(body, res);
+  })
+  .post('/pecas/populate', async (req, res)=>{
+    await pecasControllers.popularTabela(res)
+  })
+  .put('/pecas', async (req, res) => {
+    const {body: {dados, dadosNovos}} = req;
+    await pecasControllers.atualizar( dados, dadosNovos, res)
+  })
+  .delete('/pecas', (req, res) => {
+    const {body: dados} = req
+    pecasControllers.deletar(dados, res);
+  })
 
 module.exports = router;
