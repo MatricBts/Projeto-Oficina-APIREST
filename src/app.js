2baad7c694e57df85411cf7b47862f800f1c899e
Matric Bastos <matricbts@gmail.com>
2022-04-28 02:44:45 -0300

    fazendo alteração para rota base, e para rodar no heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const peças = require('./routes/pecas.routes')
 
 const routes = (app) => {
   app.route('/').get((req,res) =>{
-    res.status(200).send('teste')
+    res.status(200).send('Projeto Resilia Modulo 4 - API REST Oficina Mecânica')
   })
 
   app.use(
